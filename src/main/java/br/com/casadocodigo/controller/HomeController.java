ec267fd66692dc632281942c37d699781ea9438f
Gustavo Vieira <guustavov@gmail.com>
2018-09-26 13:31:19 -0300

    new home page

--- src/main/java/br/com/casadocodigo/controller/HomeController.java
+++ src/main/java/br/com/casadocodigo/controller/HomeController.java
@@ -1,13 +1,29 @@
 package br.com.casadocodigo.controller;
 
+import br.com.casadocodigo.dao.ProdutoDAO;
+import br.com.casadocodigo.model.Produto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    private ProdutoDAO produtoDAO;
+
     @RequestMapping("/")
-    public String home(){
-        return "home";
+    public ModelAndView home(){
+
+        ModelAndView modelAndView = new ModelAndView("home");
+
+        List<Produto> produtos = produtoDAO.listar();
+        modelAndView.addObject("produtos", produtos);
+
+        return modelAndView;
     }
+
 }
