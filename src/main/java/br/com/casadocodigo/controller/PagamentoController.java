2e4c821ae78884cc737337b3288b2f9d754ca055
Gustavo Vieira <guustavov@gmail.com>
2018-09-26 10:12:31 -0300

    payment value is now posting to herokuapp (max-value = 500)

--- src/main/java/br/com/casadocodigo/controller/PagamentoController.java
+++ src/main/java/br/com/casadocodigo/controller/PagamentoController.java
@@ -1,12 +1,15 @@
 package br.com.casadocodigo.controller;
 
 import br.com.casadocodigo.model.Carrinho;
+import br.com.casadocodigo.model.DadosPagamento;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -19,13 +22,23 @@ public class PagamentoController {
     @Autowired
     private Carrinho carrinho;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @RequestMapping(value = "/finalizar", method = RequestMethod.POST)
     public ModelAndView finalizar(RedirectAttributes model){
+        try {
+            String uri = "http://book-payment.herokuapp.com/payment";
+            String response = restTemplate.postForObject(uri, new DadosPagamento(carrinho.getTotal()), String.class);
 
-        System.out.println(carrinho.getTotal());
+            model.addFlashAttribute("sucesso", response);
+            System.out.println(response);
 
-        model.addFlashAttribute("sucesso", "Pagamento realizado com sucesso!");
-
-        return new ModelAndView("redirect:/produtos");
+            return new ModelAndView("redirect:/produtos");
+        } catch (RestClientException e) {
+            e.printStackTrace();
+            model.addFlashAttribute("falha", "Valor maior que o permitido (R$500,00)!");
+            return new ModelAndView("redirect:/produtos");
+        }
     }
 }
