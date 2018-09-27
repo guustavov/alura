e72cb68b33762db7ff908fb5fb9fbbf74db5f85c
Gustavo Vieira <guustavov@gmail.com>
2018-09-27 10:09:35 -0300

    entities to handle users and its roles

--- src/main/java/br/com/casadocodigo/model/Role.java
+++ src/main/java/br/com/casadocodigo/model/Role.java
@@ -1,4 +1,26 @@
 package br.com.casadocodigo.model;
 
-public class Role {
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Role implements GrantedAuthority {
+
+    @Id
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String getAuthority() {
+        return this.nome;
+    }
 }
