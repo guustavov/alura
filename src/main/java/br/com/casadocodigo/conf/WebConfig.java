1b8990b07c3fdb86637871dc0b1c398c4c40f42f
Gustavo Vieira <guustavov@gmail.com>
2018-10-01 13:42:56 -0300

    spring mailSender implementation (server config)

--- src/main/java/br/com/casadocodigo/conf/WebConfig.java
+++ src/main/java/br/com/casadocodigo/conf/WebConfig.java
@@ -17,6 +17,8 @@ import org.springframework.format.datetime.DateFormatter;
 import org.springframework.format.datetime.DateFormatterRegistrar;
 import org.springframework.format.support.DefaultFormattingConversionService;
 import org.springframework.format.support.FormattingConversionService;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.web.accept.ContentNegotiationManager;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartResolver;
@@ -35,6 +37,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 @Configuration
@@ -130,6 +133,25 @@ public class WebConfig extends WebMvcConfigurerAdapter{
     @Bean
     public LocaleResolver localeResolver(){
         return new CookieLocaleResolver();
+    }
+
+    @Bean
+    public MailSender mailSender(){
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setUsername("alura.springmvc@gmail.com");
+        mailSender.setPassword("alura2015");
+        mailSender.setPort(587);
+
+        Properties mailProperties = new Properties();
+        mailProperties.put("mail.smtp.auth", true);
+        mailProperties.put("mail.smtp.starttls.enable", true);
+        mailProperties.put("mail.smtp.starttls.required", true);
+
+
+        mailSender.setJavaMailProperties(mailProperties);
+        return mailSender;
     }
 
 }
