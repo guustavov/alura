46b156e6ea6e5bcdf8aea2c84ed8d62b0503b01d
Gustavo Vieira <guustavov@gmail.com>
2019-01-22 10:09:10 -0200

    implemented appraiser and its test

--- tdd-java/teste-de-unidade/src/br/com/caelum/leilao/servico/Avaliador.java
+++ tdd-java/teste-de-unidade/src/br/com/caelum/leilao/servico/Avaliador.java
@@ -1,4 +1,20 @@
 package br.com.caelum.leilao.servico;
 
+import br.com.caelum.leilao.dominio.Lance;
+import br.com.caelum.leilao.dominio.Leilao;
+
+import java.util.Comparator;
+import java.util.Optional;
+
 public class Avaliador {
+
+    private Optional<Lance> maiorLance;
+
+    public void avalia(Leilao leilao){
+        maiorLance = leilao.getLances().stream().max(Comparator.comparing(Lance::getValor));
+    }
+
+    public Lance getMaiorLance() {
+        return maiorLance.orElseThrow(RuntimeException::new);
+    }
 }
